6f66dfc1c1d1b5da4e989863f7e9eac5214ee4da
김예빈 <kyb-0215@naver.com>
2025-08-11 13:47:51 +0900

    first commit

---
+++ src/main/java/hello/chat/view/ViewController.java
@@ -0,0 +1,16 @@
+package hello.chat.view;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
+
