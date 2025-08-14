672d48b6785397f735a3f56a15fd5a46b926d66d
김예빈 <kyb-0215@naver.com>
2025-08-14 17:07:17 +0900

    loadContent는 main에서 선언

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,20 +1,3 @@
-function loadContent(url) {
-    if (!isAuthenticated) {
-        window.location.href = '/login';
-    } else {
-        $.ajax({
-            url: url,
-            type: "GET",
-            success: function (html) {
-                $("#main-content").html(html);
-            },
-            error: function (xhr, status, error) {
-                console.error("AJAX Error:", status, error);
-            }
-        });
-    }
-}
-
 // 사이드바 토글
 $('.menu-toggle').on('click', function() {
     $('nav').toggleClass('collapsed');
