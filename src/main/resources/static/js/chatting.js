f99b972e2da116d9bf64690b943ffc81e37d4b6f
김예빈 <kyb-0215@naver.com>
2025-08-22 17:32:35 +0900

    채팅화면 전체적인 css 수정

--- src/main/resources/static/js/chatting.js
+++ src/main/resources/static/js/chatting.js
@@ -18,7 +18,16 @@ function sendMessage() {
 function showMessage(message) {
     var chatBox = document.getElementById('chatBox');
     var msgDiv = document.createElement('div');
+    msgDiv.classList.add('chat-message');
+
+    if(message.sender === loginUser.name) {
+        msgDiv.classList.add('self'); // 내 메시지
+    } else {
+        msgDiv.classList.add('other'); // 상대 메시지
+    }
+
     msgDiv.textContent = message.sender + ": " + message.message;
     chatBox.appendChild(msgDiv);
     chatBox.scrollTop = chatBox.scrollHeight;
 }
+
