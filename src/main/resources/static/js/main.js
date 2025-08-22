8ec72c539db0f6589be8691e46ee221094af6565
김예빈 <kyb-0215@naver.com>
2025-08-22 17:23:04 +0900

    채팅, 챗봇 리스트 보여주기(샘플)

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -12,3 +12,45 @@ function connect() {
 }
 
 connect();
+
+const chattingRooms = [
+    { id: 1, name: "채팅1" },
+    { id: 2, name: "채팅2" },
+    { id: 3, name: "채팅3" }
+];
+
+const chatbotList = [
+    { id: "c1", name: "챗봇1" },
+    { id: "c2", name: "챗봇2" },
+    { id: "c3", name: "챗봇3" }
+];
+
+// 채팅방 리스트 보여주기
+function showChattingList() {
+    loadContent('/chatting')
+
+    const container = document.querySelector(".chatting-list");
+    container.innerHTML = ""; // 초기화
+
+    chattingRooms.forEach(room => {
+        const div = document.createElement("div");
+        div.classList.add("chatting-room-item");
+        div.textContent = room.name;
+        container.appendChild(div);
+    });
+}
+
+// 챗봇 리스트 보여주기
+function showChatbotList() {
+    loadContent('/chatbot')
+
+    const container = document.querySelector(".chatting-list");
+    container.innerHTML = ""; // 초기화
+
+    chatbotList.forEach(bot => {
+        const div = document.createElement("div");
+        div.classList.add("chatbot-item");
+        div.textContent = bot.name;
+        container.appendChild(div);
+    });
+}
