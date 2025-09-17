43d52df428ee58e6f25cd55805886ee2b1f94cab
김예빈 <kyb-0215@naver.com>
2025-09-17 11:15:46 +0900

    챗봇 삭제

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -19,18 +19,9 @@ const chattingRooms = [
     { id: 3, name: "채팅3" }
 ];
 
-const chatbotList = [
-    { id: "c1", name: "챗봇1" },
-    { id: "c2", name: "챗봇2" },
-    { id: "c3", name: "챗봇3" }
-];
-
 // 채팅방 리스트 보여주기
 function showChattingList() {
     loadContent('/chatting')
-
-    // new 채팅방 생성
-
 
     const container = document.querySelector(".chatting-list");
     container.innerHTML = ""; // 초기화
@@ -39,21 +30,6 @@ function showChattingList() {
         const div = document.createElement("div");
         div.classList.add("chatting-room-item");
         div.textContent = room.name;
-        container.appendChild(div);
-    });
-}
-
-// 챗봇 리스트 보여주기
-function showChatbotList() {
-    loadContent('/chatbot')
-
-    const container = document.querySelector(".chatting-list");
-    container.innerHTML = ""; // 초기화
-
-    chatbotList.forEach(bot => {
-        const div = document.createElement("div");
-        div.classList.add("chatbot-item");
-        div.textContent = bot.name;
         container.appendChild(div);
     });
 }
