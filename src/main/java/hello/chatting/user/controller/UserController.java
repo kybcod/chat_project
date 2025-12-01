094a48d5a58a51ec689e05e4e6bf26840e1dd990
김예빈 <kyb-0215@naver.com>
2025-12-01 10:54:02 +0900

    친구 리스트 가져오기

---
+++ src/main/java/hello/chatting/user/controller/UserController.java
@@ -0,0 +1,22 @@
+package hello.chatting.user.controller;
+
+import hello.chatting.user.domain.User;
+import hello.chatting.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("users")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> userList() {
+        return userService.findAll();
+    }
+}
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -12,3 +12,39 @@ function connect() {
 }
 
 connect();
+
+function showFriendList() {
+    $.ajax({
+        url: "/users",
+        type: "GET",
+        success: function (friends) {
+            const containerFri = document.querySelector(".friends-list");
+            containerFri.innerHTML = "";
+
+            friends.forEach(fri => {
+                const div = document.createElement("div");
+                div.classList.add("friends-item");
+
+                // 🔹 프로필 이미지
+                const img = document.createElement("img");
+                img.src = fri.profileImage || "/images/orgProfile.png";
+                img.alt = fri.name;
+                img.classList.add("friends-profile-img");
+
+                // 🔹 이름
+                const nameSpan = document.createElement("span");
+                nameSpan.textContent = fri.name;
+                nameSpan.classList.add("friends-name");
+
+                // div 구성
+                div.appendChild(img);
+                div.appendChild(nameSpan);
+
+                containerFri.appendChild(div);
+            });
+        },
+        error: function (xhr, status, error) {
+            console.error("Error:", status, error);
+        }
+    });
+}
