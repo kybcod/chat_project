5b749c317a1c14290fdc0b0721d84880c9ebe9e9
김예빈 <kyb-0215@naver.com>
2025-12-01 10:41:27 +0900

    전체적인 채팅, 친구 ui 변경

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -12,24 +12,3 @@ function connect() {
 }
 
 connect();
-
-const chattingRooms = [
-    { id: 1, name: "채팅1" },
-    { id: 2, name: "채팅2" },
-    { id: 3, name: "채팅3" }
-];
-
-// 채팅방 리스트 보여주기
-function showChattingList() {
-    loadContent('/chatting')
-
-    const container = document.querySelector(".chatting-list");
-    container.innerHTML = ""; // 초기화
-
-    chattingRooms.forEach(room => {
-        const div = document.createElement("div");
-        div.classList.add("chatting-room-item");
-        div.textContent = room.name;
-        container.appendChild(div);
-    });
-}
